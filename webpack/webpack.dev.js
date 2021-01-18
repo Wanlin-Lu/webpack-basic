1639a12e1a6f25447230afc676ba69271943bee6
Wanlin-Lu <luwanlin@126.com>
2021-01-18 10:31:49 +0800

    webpack config

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,18 @@
+const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
+	mode: 'development',
 	devtool: 'inline-source-map',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname,'../dist'),
+		publicPath: '/'
+	},
 	devServer: {
-		contentBase: './dist'
+		contentBase: 'dist',
+		inline: true,
+		port: 9000
 	}
 })
